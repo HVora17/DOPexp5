f81b36b73a6f506e3319a11747f7e102cbcabdf0
HVora17 <hvora1711@gmail.com>
2021-09-22 19:45:25 +0530

    2

--- fibonacci.java
+++ fibonacci.java
@@ -1,16 +1,15 @@
 public class fibonacci 
 {
     public static void main(String[] args) {
-        int n,a=0,b=0,c=1;
-        n=Integer.parseInt(args[0]);
+        int num,a=0,b=0,c=1;
+        num=Integer.parseInt(args[0]);
         System.out.print("Fibonacci Series: ");
-        for(int i=1;i<=n;i++)
+        for(int i=1;i<=num;i++)
         {
             a=b;
             b=c;
             c=a+b;
             System.out.print(a+" ");
-            
         }
     }    
 }
